5829001393ce3fc08a550569b2476c283630c75b
ta-Huyen <tahuyen151@gmail.com>
2024-07-31 08:40:26 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let isEdit = false;
+
 document.addEventListener('DOMContentLoaded', function () {
     fetch("data.json")
     .then (function (response) {
@@ -23,16 +25,20 @@ function saveData() {
         hometown = document.getElementById("hometown").value.trim();
 
     if (validationData()) {
-        let checkboxes = document.querySelectorAll('.student-checkbox');
-        let checkedCheckboxes = Array.from(checkboxes).filter(cb => cb.checked);
-    
-        if (checkedCheckboxes.length == 0) {
-            addRowToTable(name, birthday, phone, hometown);
+        if (isEdit) {
+            let checkboxes = document.querySelectorAll('.student-checkbox');
+            let checkedCheckboxes = Array.from(checkboxes).filter(cb => cb.checked);
+
+            if (checkedCheckboxes.length > 0) {
+                checkedCheckboxes[0].parentNode.parentNode.cells[1].innerHTML = name;
+                checkedCheckboxes[0].parentNode.parentNode.cells[2].innerHTML = birthday;
+                checkedCheckboxes[0].parentNode.parentNode.cells[3].innerHTML = phone;
+                checkedCheckboxes[0].parentNode.parentNode.cells[4].innerHTML = hometown;
+            }
+            
+            isEdit = false;
         } else {
-            checkedCheckboxes[0].parentNode.parentNode.cells[1].innerHTML = name;
-            checkedCheckboxes[0].parentNode.parentNode.cells[2].innerHTML = birthday;
-            checkedCheckboxes[0].parentNode.parentNode.cells[3].innerHTML = phone;
-            checkedCheckboxes[0].parentNode.parentNode.cells[4].innerHTML = hometown;
+            addRowToTable(name, birthday, phone, hometown);
         }
         // writeJson(tableToJson);
         resetData();
@@ -71,6 +77,8 @@ function editData() {
         document.getElementById("birthday").value = checkedCheckboxes[0].parentNode.parentNode.cells[2].innerHTML;
         document.getElementById("phone").value = checkedCheckboxes[0].parentNode.parentNode.cells[3].innerHTML;
         document.getElementById("hometown").value = checkedCheckboxes[0].parentNode.parentNode.cells[4].innerHTML;
+
+        isEdit = true;
     }     
 };
 
